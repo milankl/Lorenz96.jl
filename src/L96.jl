45576daa32559c4a006e20a58f7ca545774f12e0
milankl <milankloewer@gmx.de>
2019-07-22 17:57:52 +0100

    Initial commit

---
+++ src/L96.jl
@@ -0,0 +1,27 @@
+function L96(::Type{T}=Float64;
+            N::Int=10_000,
+            n::Int=36,
+            X::Array{Float64,1}=zeros(36),
+            F::Float64=8.0,
+            s::Float64=1.0,
+            Δt::Float64=0.002,
+            η::Float64=0.01,
+            scheme::String="RK4") where {T<:AbstractFloat}
+
+            # if both n and X are specified then they should match!
+            if n != 36 && X != zeros(36) && length(X) != n
+                throw(error("n=$n and length(X)=$(length(X)) do not match."))
+            end
+
+            # add the forcing for equilibrium initial conditions
+            if X == zeros(36)   # the default
+                X = zeros(n) .+ F         
+                X[1] += η # add small perturbation on the first variable
+            end
+
+            if scheme == "RK4"
+                return RK4(T,N,X,F,s,Δt)
+            else
+                throw(error("Other schemes than RK4 not implemented yet."))
+            end
+end
---
+++ src/time_integration.jl
@@ -0,0 +1,60 @@
+function RK4(T::Type,N::Int,X::Array{Float64,1},F::Float64,s::Float64,Δt::Float64)
+
+    # number of variables
+    n = length(X)
+
+    # scale the initial conditions
+    for i = 1:n
+        X[i] = s*X[i]
+    end
+
+    # preallocate for storing results - store without scaling in Float64
+    Xout = Array{Float64,2}(undef,n,N+1)
+    Xout[:,1] = X/s
+
+    # Runge Kutta 4th order coefficients including time step and sigma for x
+    RKα = [1/6.,1/3.,1/3.,1/6.]*Δt
+    RKβ = [1/2.,1/2.,1.]*Δt
+
+    # convert everything to the desired number system determined by T
+    X = T.(X)
+    F = T.(F*s)
+    s_inv = T(1.0 / s)
+    RKα = T.(RKα)
+    RKβ = T.(RKβ)
+
+    # preallocate memory for intermediate results
+    X0 = deepcopy(X)
+    X1 = deepcopy(X)
+    dX = zero(X)       # tendencies
+
+    for i = 1:N
+        for j in 1:n
+            @inbounds X1[j] = X[j]
+        end
+
+        for rki = 1:4
+            rhs!(dX,X1,F,s_inv)
+
+            if rki < 4
+                for j in 1:n
+                    @inbounds X1[j] = X[j] + dX[j] * RKβ[rki]
+                end
+            end
+
+            # sum the RK steps on the go
+            for j in 1:n
+                @inbounds X0[j] += dX[j] * RKα[rki]
+            end
+        end
+
+        for j in 1:n
+            @inbounds X[j] = X0[j]
+        end
+
+        # store as 64bit, undo scaling
+        Xout[:,i+1] = Float64.(X)/s
+    end
+
+    return Xout
+end
