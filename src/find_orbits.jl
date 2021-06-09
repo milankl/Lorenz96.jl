a61ee577ea450ecb5d96ba348d303a997c6e73bf
Milan <milankloewer@gmx.de>
2021-06-09 13:33:18 +0100

    log2 scaling for dynamic test

--- src/find_orbits.jl
+++ src/find_orbits.jl
@@ -6,7 +6,8 @@ Recursive function that starts testing for orbits over period length l,
 but enlarges that testing period if no orbit was found."""
 function orbit_length(  X0::Vector{T};          # initial condition
                         lspinup::Int=100_000,   # spinup
-                        lchunk::Int=1000        # chunk size
+                        lchunk::Int=100_000,    # chunk size
+                        verbose::Bool=true
                         ) where T      
     
     X0 = L96(T,X=X0,n=length(X0),N=lspinup)
@@ -36,18 +37,23 @@ function orbit_length(  X0::Vector{T};          # initial condition
         nchunk += 1
 
         # move Xtest2 slower than chunks forward in time 
-        if sqrt(nchunk) % 1 == 0.0
+        if log2(nchunk) % 1 == 0.0
             Xtest2 = copy(X0)
             ltest2 = nchunk*lchunk
         end
     end
+
+    if verbose
+        println("Orbit of length=$l found after $nchunk chunks of size $lchunk.")
+    end
+
     return l,X0    # return orbit length and an X that's on the orbit
 end
 
 """Returns the minimum X on the orbit of length l, starting from X0 on the orbit."""
 function orbit_minimum( X0::Vector{T},               # initial condition on the orbit
                         l::Int;                      # the period length of the orbit
-                        lchunk::Int=1000) where T     # chunk size to reduce memory stress
+                        lchunk::Int=100_000) where T     # chunk size to reduce memory stress
     
     Xmin = copy(X0)         # start with X0 as minimum
     Xnorm = norm(Xmin)
@@ -75,8 +81,9 @@ end
 """Returns both the length and minimum of the orbit reached from initial condition X0."""
 function orbit_length_minimum(  X0::Vector{T};              # initial condition on the orbit
                                 lspinup::Int=100_000,       # spin-up length
-                                lchunk::Int=1000) where T    # chunk size
-    len,X = orbit_length(X0;lspinup,lchunk)    
+                                lchunk::Int=100_000,
+                                verbose::Bool=true) where T    # chunk size
+    len,X = orbit_length(X0;lspinup,lchunk,verbose)    
     Xmin = orbit_minimum(X,len;lchunk)
     return Orbit(len,Xmin)
 end
@@ -88,7 +95,7 @@ function find_orbits(   ::Type{T},                  # Number format
                         n::Int=100;                 # number of initial conditions
                         lini::Int=100_000,          # initial spin-up length in Float64
                         lspinup::Int=100_000,       # spinup with format T
-                        lchunk::Int=1000,            # chunk size to reduce memory stress
+                        lchunk::Int=100_000,        # chunk size to reduce memory stress
                         verbose::Bool=true          # report every orbit found?
                         ) where T         # n initial conditions
 
@@ -104,7 +111,7 @@ function find_orbits(   ::Type{T},                  # Number format
     tic = time()
     orbits = @distributed (reduce_orbits) for i in 1:n            # for n ICs calculate orbit lengths & x
         X = convert(Vector{T},ini[:,i])
-        orbit = orbit_length_minimum(X;lspinup,lchunk)
+        orbit = orbit_length_minimum(X;lspinup,lchunk,verbose)
         if verbose
             println(orbit)
         end
